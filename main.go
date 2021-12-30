70868585376030b4991a97aaa0fdf74c3332db74
t.fukumoto <t.fukumoto@gamewith.co.jp>
2021-12-30 19:32:16 +0900

    変数名整理

--- main.go
+++ main.go
@@ -37,20 +37,20 @@ func main() {
 			return
 		}
 		respBody, _ := ioutil.ReadAll(resp.Body)
+		resp.Body.Close()
 		if resp.StatusCode != 200 {
 			w.WriteHeader(resp.StatusCode)
 			w.Write(respBody)
 			return
 		}
-		defer resp.Body.Close()
 
-		img, src_image_type, err := image.Decode(bytes.NewReader(respBody))
+		src_img, src_image_type, err := image.Decode(bytes.NewReader(respBody))
 		if err != nil {
 			w.WriteHeader(500)
 			w.Write([]byte(err.Error()))
 			return
 		}
-		// agif/apngは処理しない #2
+		// agif/apngは処理しない issue#2
 		if src_image_type == "gif" {
 			tmpimg, _ := gif.DecodeAll(bytes.NewReader(respBody))
 			if (len(tmpimg.Image) > 1) {
@@ -71,17 +71,19 @@ func main() {
 		var dst *image.RGBA
 
 		// まずリサイズ
-		rct := img.Bounds()
+		rct := src_img.Bounds()
 		if (rct.Dx() > max_width) {
 			dst_width := float64(max_width)
 			dst_height := math.Ceil(dst_width / float64(rct.Dx() * rct.Dy()))
 
 			dst = image.NewRGBA(image.Rect(0, 0, int(dst_width), int(dst_height)))
-			draw.CatmullRom.Scale(dst, dst.Bounds(), img, img.Bounds(), draw.Over, nil)
+			draw.CatmullRom.Scale(dst, dst.Bounds(), src_img, src_img.Bounds(), draw.Over, nil)
 		} else {
 			dst = image.NewRGBA(image.Rect(0, 0, rct.Dx(), rct.Dy()))
-			draw.Copy(dst,image.Point{0,0}, img, img.Bounds(), draw.Over, nil)
+			draw.Copy(dst,image.Point{0,0}, src_img, src_img.Bounds(), draw.Over, nil)
 		}
+		// 気休め。
+		src_img = nil
 
 		w.Header().Add("X-imtest-convert_width", strconv.Itoa(dst.Bounds().Dx()))
 		w.Header().Add("X-imtest-convert_height", strconv.Itoa(dst.Bounds().Dy()))
