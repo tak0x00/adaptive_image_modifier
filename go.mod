d1eee0cef8e174adbd6722f384adabdaa58bf9af
t.fukumoto <t.fukumoto@gamewith.co.jp>
2021-12-30 14:42:45 +0900

    ざっくりリサイズ＆変換できるまで

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module main
 
 go 1.13
+
+require (
+	github.com/harukasan/go-libwebp v0.0.0-20190703060927-68562c9c99af
+	golang.org/x/image v0.0.0-20211028202545-6944b10bf410
+)
--- main.go
+++ main.go
@@ -1,8 +1,15 @@
 package main
 
 import (
+	"math"
+	"strconv"
     "net/http"
-	"io/ioutil"
+    "image"
+	"image/png"
+	"image/gif"
+	"image/jpeg"
+    "golang.org/x/image/draw"
+    "github.com/harukasan/go-libwebp/webp"
 )
 
 func main() {
@@ -11,15 +18,79 @@ func main() {
 		origin_domain := r.URL.Query().Get("origin_domain")
 		path := r.URL.Path
 		resize_mode := r.URL.Query().Get("resize_mode")
-		max_width := r.URL.Query().Get("resize_resolution")
+		max_width_string := r.URL.Query().Get("resize_resolution")
+		max_width, _ := strconv.Atoi(max_width_string)
 
-		resp, _ := http.Get("https://" + origin_domain + path)
+
+		resp, err := http.Get("https://" + origin_domain + path)
+		if err != nil {
+			w.WriteHeader(500)
+			w.Write([]byte(err.Error()))
+			return
+		}
 		defer resp.Body.Close()
-		byteArray, _ := ioutil.ReadAll(resp.Body)
+
+		img, src_image_type, err := image.Decode(resp.Body)
+		if err != nil {
+			w.WriteHeader(500)
+			w.Write([]byte(err.Error()))
+			return
+		}
+
+		var dst *image.RGBA
+
+		// まずリサイズ
+		rct := img.Bounds()
+		if (rct.Dx() > max_width) {
+			dst_width := float64(max_width)
+			dst_height := math.Ceil(dst_width / float64(rct.Dx() * rct.Dy()))
+
+			dst = image.NewRGBA(image.Rect(0, 0, int(dst_width), int(dst_height)))
+			draw.CatmullRom.Scale(dst, dst.Bounds(), img, img.Bounds(), draw.Over, nil)
+		} else {
+			dst = image.NewRGBA(image.Rect(0, 0, rct.Dx(), rct.Dy()))
+			draw.Copy(dst,image.Point{0,0}, img, img.Bounds(), draw.Over, nil)
+		}
 
 		w.Header().Add("X-imtest-mode", resize_mode)
-		w.Header().Add("X-imtest-max_width", max_width)
-        w.Write(byteArray)
+		w.Header().Add("X-imtest-max_width", max_width_string)
+		w.Header().Add("X-imtest-convert_width", strconv.Itoa(dst.Bounds().Dx()))
+		w.Header().Add("X-imtest-convert_height", strconv.Itoa(dst.Bounds().Dy()))
+
+
+		// 形式変換
+		dst_image_type := src_image_type;
+		switch (resize_mode) {
+		case "iphone":
+			// 変換なし
+
+		// https://developer.android.com/guide/topics/media/media-formats?hl=ja
+		case "androidaosp":
+			// ~android 5
+			// 変換なし
+
+		case "androidchrome":
+			// android 6 ~
+			dst_image_type = "webp"
+
+		case "pc":
+			dst_image_type = "webp"
+
+		default:
+			// 変換なし
+		}
+
+		switch (dst_image_type) {
+		case "jpeg":
+			jpeg.Encode(w, dst, &jpeg.Options{Quality: 100})
+		case "png":
+			png.Encode(w, dst)
+		case "gif":
+			gif.Encode(w, dst, nil)
+		case "webp":
+			con, _ := webp.ConfigPreset(webp.PresetDefault, 80)
+			err = webp.EncodeRGBA(w, dst, con)
+		}
 	})
     http.ListenAndServe(":8080", mux)
 }
