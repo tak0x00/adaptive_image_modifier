185690ad9ae937bf3015f30854bd696b4856f1f5
tak0x00 <tak+github@monooki.info>
2022-03-25 17:41:49 +0900

    varnish少しチューニング

--- varnish/entrypoint.sh
+++ varnish/entrypoint.sh
@@ -6,6 +6,11 @@
     -a proxy=:8443,PROXY \
     -p feature=+http2 \
     -s malloc,$VARNISH_SIZE \
+    -p thread_pools=4 \
+    -p thread_pool_min=200 \
+    -p thread_pool_max=4000 \
+    -p thread_pool_add_delay=2 \
+    -p listen_depth=4096 \
     "$@"
 
 varnishlog -i VCL_Log
